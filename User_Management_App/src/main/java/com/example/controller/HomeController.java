de0fa6e5a2d3211f446c56da863f3316fab35dc1
IgirubElam <igirubuntuelam@gmail.com>
2024-02-02 10:08:29 +0200

    commit

--- User_Management_App/src/main/java/com/example/controller/HomeController.java
+++ User_Management_App/src/main/java/com/example/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.example.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.model.UserDtls;
+import com.example.repository.UserRepository;
 import com.example.service.UserService;
 
 @Controller
@@ -15,6 +19,22 @@ public class HomeController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@ModelAttribute
+	private void userDetails(Model m, Principal p) {
+		
+		if(p!=null)
+		{
+			String email = p.getName();
+			UserDtls user = userRepository.findByEmail(email);
+			m.addAttribute("user", user);
+		}
+		
+		
+	}
 	
 	@GetMapping("/")
 	public String index()
--- User_Management_App/src/main/java/com/example/controller/UserController.java
+++ User_Management_App/src/main/java/com/example/controller/UserController.java
@@ -3,11 +3,15 @@ package com.example.controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.model.UserDtls;
 import com.example.repository.UserRepository;
@@ -18,6 +22,9 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@ModelAttribute
 	private void userDetails(Model m, Principal p) {
@@ -31,5 +38,42 @@ public class UserController {
 	@GetMapping("/")
 	public String home() {
 		return "user/home";
+	}
+	
+	@GetMapping("/changePass")
+	public String loadChangePassword() {
+		return "user/change_password";
+	}
+	
+	@PostMapping("/updatePassword")
+	public String changePassword(Principal p, @RequestParam("oldPass") String oldPass, 
+			@RequestParam("newPass") String newPass, RedirectAttributes redirectAttributes) {
+		
+		String email = p.getName();
+		
+		UserDtls loginUser = userRepository.findByEmail(email);
+		
+		boolean changingPassword = passwordEncoder.matches(oldPass, loginUser.getPassword());
+		
+		if(changingPassword) {
+			
+			loginUser.setPassword(passwordEncoder.encode(newPass));
+			UserDtls updatePasswordUser = userRepository.save(loginUser);
+			
+			if(updatePasswordUser!=null) {
+				
+				redirectAttributes.addFlashAttribute("success", "Password change success");
+				
+			}else {
+				
+				redirectAttributes.addFlashAttribute("error", "Something wrong on server");
+				
+			}
+			
+		}else {
+			redirectAttributes.addFlashAttribute("error", "Old Password incorrect");
+		}
+		
+		return "redirect:/user/changePass";
 	}
 }
